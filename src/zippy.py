84064f461fd8af92aae7fa441a2d3cebadee1583
FII14 <121723975+FII14@users.noreply.github.com>
2023-09-10 07:10:01 +0700

    Update zippy.py

--- src/zippy.py
+++ src/zippy.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 zip_file = input("Enter the ZIP file name: ")
 wordlist_file = input("Enter the wordlist file name: ")
 
-with open(wordlist_file, 'r') as wordlist:
+with open(wordlist_file, 'r', encoding="utf-8", errors="ignore") as wordlist:
     password_list = wordlist.readlines()
 
 with tqdm(total=len(password_list)) as pbar:
