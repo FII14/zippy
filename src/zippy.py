3c73f722487372073160fc9f64028dd3c1bc42aa
FII14 <121723975+FII14@users.noreply.github.com>
2023-09-10 07:08:05 +0700

    Create zippy.py

---
+++ src/zippy.py
@@ -0,0 +1,21 @@
+import zipfile
+from tqdm import tqdm
+
+zip_file = input("Enter the ZIP file name: ")
+wordlist_file = input("Enter the wordlist file name: ")
+
+with open(wordlist_file, 'r') as wordlist:
+    password_list = wordlist.readlines()
+
+with tqdm(total=len(password_list)) as pbar:
+    for line in password_list:
+        password = line.strip()
+        try:
+            with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+                zip_ref.extractall(pwd=password.encode())
+            print(f"Password found: {password}")
+            break
+        except Exception as e:
+            pbar.update(1)
+    else:
+        print("No matching password found in the wordlist.")
