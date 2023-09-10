71dd7b913ac13c9beb8d857f192fec1a3cb2e69a
FII14 <121723975+FII14@users.noreply.github.com>
2023-09-10 08:39:12 +0700

    Update zippy.py

--- src/zippy.py
+++ src/zippy.py
@@ -8,13 +8,13 @@ file_wordlist = "wordlist/rockyou txt"
 with open(file_zip, "rb") as fz:
     fz = zipfile.ZipFile(file_zip)
 
-with open(file_wordlist, "r", encoding="utf-8", errors="ignore") as fw:
+with open(file_wordlist, "r", encoding="latin-1", errors="ignore") as fw:
     daftar_kata_sandi = fw.readlines()
 
 for kata_sandi in tqdm(daftar_kata_sandi, desc="Progress", ncols=100, ascii=True):
     kata_sandi = kata_sandi.strip()
     try:
-        zip_file.extractall(pwd=kata_sandi.encode("utf-8"))
+        zip_file.extractall(pwd=kata_sandi.encode("latin-1"))
         print(f"[+] Kata Sandi ditemukan: {kata_sandi}")
         break
     except Exception:
