74dbdecbbdddf20f9f60bc1f4c003fee6c79acc5
FII14 <121723975+FII14@users.noreply.github.com>
2023-09-10 08:34:53 +0700

    Update zippy.py

--- src/zippy.py
+++ src/zippy.py
@@ -2,36 +2,22 @@ import zipfile
 from tqdm import tqdm
 import os
 
-zip_file_name = input("Enter the ZIP file name: ")
+file_zip = input("Masukkan nama file zip $ ")
+file_wordlist = "wordlist/rockyou txt"
 
-if not os.path.exists(zip_file_name):
-    print("ZIP file not found.")
-    exit(1)
-    
-wordlist_name = input("Enter the custom wordlist file name: ")
+with open(file_zip, "rb") as fz:
+    fz = zipfile.ZipFile(file_zip)
 
-if not os.path.exists(wordlist_name):
-    print("Custom wordlist file not found.")
-    exit(1)
+with open(file_wordlist, "r", encoding="utf-8", errors="ignore") as fw:
+    daftar_kata_sandi = fw.readlines()
 
-try:
-    with open(zip_file_name, "rb") as zip_file:
-        zip_file = zipfile.ZipFile(zip_file)
-
-    with open(wordlist_name, "r", encoding="utf-8", errors="ignore") as wordlist_file:
-        passwords = wordlist_file.readlines()
-except Exception as e:
-    print("An error occurred while opening files:", e)
-    exit(1)
-
-for password in tqdm(passwords, desc="Progress", ncols=100, ascii=True):
-    password = password.strip()
+for kata_sandi in tqdm(daftar_kata_sandi, desc="Progress", ncols=100, ascii=True):
+    kata_sandi = kata_sandi.strip()
     try:
-        zip_file.extractall(pwd=password.encode("utf-8"))
-        print(f"Password found: {password}")
+        zip_file.extractall(pwd=kata_sandi.encode("utf-8"))
+        print(f"[+] Kata Sandi ditemukan: {kata_sandi}")
         break
-    except Exception as e:
+    except Exception:
         pass
 else:
-    print("No matching password found.")
-    
+    print("Kata sandi tidak ditemukan di dalam file wordlist.")
