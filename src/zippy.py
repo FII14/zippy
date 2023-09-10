0459a9197b18af2dea1585c6d3a9790c5a20e4f2
FII14 <121723975+FII14@users.noreply.github.com>
2023-09-10 08:18:28 +0700

    Update zippy.py

--- src/zippy.py
+++ src/zippy.py
@@ -1,21 +1,37 @@
 import zipfile
 from tqdm import tqdm
+import os
 
-zip_file = input("Enter the ZIP file name: ")
-wordlist_file = input("Enter the wordlist file name: ")
+zip_file_name = input("Enter the ZIP file name: ")
 
-with open(wordlist_file, 'r', encoding="utf-8", errors="ignore") as wordlist:
-    password_list = wordlist.readlines()
+if not os.path.exists(zip_file_name):
+    print("ZIP file not found.")
+    exit(1)
+    
+wordlist_name = input("Enter the custom wordlist file name: ")
 
-with tqdm(total=len(password_list)) as pbar:
-    for line in password_list:
-        password = line.strip()
-        try:
-            with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-                zip_ref.extractall(pwd=password.encode())
-            print(f"Password found: {password}")
-            break
-        except Exception as e:
-            pbar.update(1)
-    else:
-        print("No matching password found in the wordlist.")
+if not os.path.exists(wordlist_name):
+    print("Custom wordlist file not found.")
+    exit(1)
+
+try:
+    with open(zip_file_name, "rb") as zip_file:
+        zip_file = zipfile.ZipFile(zip_file)
+
+    with open(wordlist_name, "r") as wordlist_file:
+        passwords = wordlist_file.readlines()
+except Exception as e:
+    print("An error occurred while opening files:", e)
+    exit(1)
+
+for password in tqdm(passwords, desc="Progress", ncols=100, ascii=True):
+    password = password.strip()
+    try:
+        zip_file.extractall(pwd=password.encode())
+        print(f"Password found: {password}")
+        break
+    except Exception as e:
+        pass
+else:
+    print("No matching password found.")
+    
