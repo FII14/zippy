4221b3d0f9eb05967f5f0db3143849a7ef5e6a81
FII14 <121723975+FII14@users.noreply.github.com>
2023-09-10 08:21:44 +0700

    Update zippy.py

--- src/zippy.py
+++ src/zippy.py
@@ -18,7 +18,7 @@ try:
     with open(zip_file_name, "rb") as zip_file:
         zip_file = zipfile.ZipFile(zip_file)
 
-    with open(wordlist_name, "r") as wordlist_file:
+    with open(wordlist_name, "r", encoding="utf-8", errors="ignore") as wordlist_file:
         passwords = wordlist_file.readlines()
 except Exception as e:
     print("An error occurred while opening files:", e)
@@ -27,7 +27,7 @@ except Exception as e:
 for password in tqdm(passwords, desc="Progress", ncols=100, ascii=True):
     password = password.strip()
     try:
-        zip_file.extractall(pwd=password.encode())
+        zip_file.extractall(pwd=password.encode("utf-8"))
         print(f"Password found: {password}")
         break
     except Exception as e:
